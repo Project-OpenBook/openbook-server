3c6b023991c9542973981afeaec124cfcb4648a4
siwon <muncool39@gmail.com>
2024-05-14 09:39:06 +0900

    Feat: event domain entity
    - event
    - eventLayout
    - eventLayoutArea
    - eventTag
    - eventTagDetail

---
+++ src/main/java/com/openbook/openbook/event/dto/EventLayoutType.java
@@ -0,0 +1,11 @@
+package com.openbook.openbook.event.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum EventLayoutType {
+    ALPHABET,
+    NUMBER
+}
---
+++ src/main/java/com/openbook/openbook/event/entity/EventLayout.java
@@ -0,0 +1,41 @@
+package com.openbook.openbook.event.entity;
+
+import com.openbook.openbook.event.dto.EventLayoutType;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class EventLayout {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String imageUrl;
+
+    @Enumerated(EnumType.STRING)
+    private EventLayoutType type;
+
+    @OneToMany(mappedBy = "linkedEventLayout", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<EventLayoutArea> areas = new ArrayList<>();
+
+    @Builder
+    public EventLayout(String imageUrl, EventLayoutType type) {
+        this.imageUrl = imageUrl;
+        this.type = type;
+    }
+}

36b92f12ce502e743a8095797eed01b6675bd990
시원 <105481797+muncool39@users.noreply.github.com>
2024-05-15 19:34:37 +0900

    Feat: 회원가입 API (#7)
    * Build: 객체 검증 의존성 추가
    * Add: security config
    * Add: password encoder config
    * Feat: user signup api

---
+++ src/main/java/com/openbook/openbook/user/UserRepository.java
@@ -0,0 +1,13 @@
+package com.openbook.openbook.user;
+
+
+import com.openbook.openbook.user.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+}

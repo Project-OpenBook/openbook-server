8c0f089422560eb1a23dd48e623a7afe98708b2b
siwon <muncool39@gmail.com>
2024-05-15 15:41:34 +0900

    Add: security config

---
+++ src/main/java/com/openbook/openbook/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.openbook.openbook.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/signup").permitAll();
+            auth.anyRequest().authenticated();
+        });
+
+        return http.build();
+    }
+}

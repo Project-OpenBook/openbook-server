5fb25423333efdd72b3445563177216f26b663fe
siwon <muncool39@gmail.com>
2024-05-16 23:25:16 +0900

    Fix: 회원가입 기능 중복 이메일 에러 처리

--- src/main/java/com/openbook/openbook/user/UserService.java
+++ src/main/java/com/openbook/openbook/user/UserService.java
@@ -1,10 +1,12 @@
 package com.openbook.openbook.user;
 
 
+import com.openbook.openbook.global.exception.OpenBookException;
 import com.openbook.openbook.user.dto.SignUpRequest;
 import com.openbook.openbook.user.dto.UserRole;
 import com.openbook.openbook.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,7 +21,7 @@ public class UserService {
     @Transactional
     public void signup(final SignUpRequest request) {
         if(userRepository.findByEmail(request.email()).isPresent()) {
-            throw new RuntimeException("중복된 이메일");
+            throw new OpenBookException(HttpStatus.CONFLICT, "이미 존재하는 이메일입니다.");
         }
         User user = User.builder()
                 .email(request.email())

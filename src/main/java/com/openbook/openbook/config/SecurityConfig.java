860e80d8f310f9e42d19f3db858db128e7c6fb70
siwon <muncool39@gmail.com>
2024-05-18 07:22:41 +0900

    Add: 인증 에러 처리 추가

--- src/main/java/com/openbook/openbook/config/SecurityConfig.java
+++ src/main/java/com/openbook/openbook/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.openbook.openbook.config;
 
 import com.openbook.openbook.global.exception.CustomAccessDeniedHandler;
+import com.openbook.openbook.global.exception.CustomAuthenticationEntryPoint;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,19 +9,24 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     private final CustomAccessDeniedHandler customAccessDeniedHandler;
+    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
                 .httpBasic(AbstractHttpConfigurer::disable)
-                .exceptionHandling(exception->{
+                .exceptionHandling(exception-> {
+                    exception.authenticationEntryPoint(customAuthenticationEntryPoint);
                     exception.accessDeniedHandler(customAccessDeniedHandler);
                 });
 

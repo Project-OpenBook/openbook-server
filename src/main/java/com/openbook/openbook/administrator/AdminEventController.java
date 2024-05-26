9c7dfd3516d30f662a1ed5e56f74842aa309360f
시원 <105481797+muncool39@users.noreply.github.com>
2024-05-26 20:52:16 +0900

    Feat: 행사 상태 변경 API (#33)
    * Add: event status update request dto
    * Feat: 행사 등록 승인 및 거부 기능

--- src/main/java/com/openbook/openbook/administrator/AdminEventController.java
+++ src/main/java/com/openbook/openbook/administrator/AdminEventController.java
@@ -2,13 +2,19 @@ package com.openbook.openbook.administrator;
 
 
 import com.openbook.openbook.administrator.dto.AdminEventData;
+import com.openbook.openbook.administrator.dto.request.EventStatusUpdateRequest;
 import com.openbook.openbook.global.dto.PageResponse;
+import com.openbook.openbook.global.dto.ResponseMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +29,14 @@ public class AdminEventController {
     public ResponseEntity<PageResponse<AdminEventData>> getEventPage(@RequestParam(defaultValue = "all") String status,
                                                                      @PageableDefault(size = 10) Pageable pageable) {
         return ResponseEntity.ok(PageResponse.of(adminEventService.getRequestedEvents(pageable, status)));
+    }
+
+    @PreAuthorize("authentication.name == '1'")
+    @PutMapping("/admin/events/{eventId}/status")
+    public ResponseEntity<ResponseMessage> changeEventStatus (@PathVariable Long eventId,
+                                                              @RequestBody EventStatusUpdateRequest request) {
+        adminEventService.changeEventStatus(eventId, request.status());
+        return ResponseEntity.ok(new ResponseMessage("행사 상태가 변경되었습니다."));
     }
 
 }

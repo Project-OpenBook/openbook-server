95b4cea40e47636853887569de55f4d1b94becfb
gitseoyeon <tjdus553926@gmail.com>
2024-05-27 20:39:27 +0900

    Remove: 불필요한 import문 제거

--- src/main/java/com/openbook/openbook/basicuser/service/UserEventLayoutAreaService.java
+++ src/main/java/com/openbook/openbook/basicuser/service/UserEventLayoutAreaService.java
@@ -11,9 +11,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import com.openbook.openbook.global.exception.OpenBookException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service

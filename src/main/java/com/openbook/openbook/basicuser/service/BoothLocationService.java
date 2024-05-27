9a93c8fe7f3619874e1581321f900993603e2d2a
gitseoyeon <tjdus553926@gmail.com>
2024-05-28 01:32:35 +0900

    Remove: 불필요한 클래스 제거

--- src/main/java/com/openbook/openbook/basicuser/service/BoothLocationService.java
+++
@@ -1,8 +0,0 @@
-package com.openbook.openbook.basicuser.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-@Service
-@RequiredArgsConstructor
-public class BoothLocationService {
-}

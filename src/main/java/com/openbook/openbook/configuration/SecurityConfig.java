b2a7f7259665e8457c20f9ce813071a8966b7dba
gitseoyeon <tjdus553926@gmail.com>
2024-05-30 00:27:46 +0900

    Feat: CORS 문제를 해결하기 위한 메서드 구현

--- src/main/java/com/openbook/openbook/configuration/SecurityConfig.java
+++ src/main/java/com/openbook/openbook/configuration/SecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @RequiredArgsConstructor
@@ -43,4 +45,17 @@ public class SecurityConfig {
             "/login",
             "/signup"
     };
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedOriginPatterns("*")
+                        .allowedMethods("*");
+            }
+        };
+    }
 }

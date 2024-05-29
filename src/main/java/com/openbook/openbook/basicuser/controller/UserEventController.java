6f73f9aa8988359837ca3c531559f3b61b2d044c
siwon <muncool39@gmail.com>
2024-05-29 15:13:50 +0900

    Feat: 진행 상태 별 행사 목록 조회

--- src/main/java/com/openbook/openbook/basicuser/controller/UserEventController.java
+++ src/main/java/com/openbook/openbook/basicuser/controller/UserEventController.java
@@ -1,18 +1,25 @@
 package com.openbook.openbook.basicuser.controller;
 
 
+import com.openbook.openbook.basicuser.dto.response.EventBasicData;
 import com.openbook.openbook.basicuser.dto.response.EventLayoutStatus;
 import com.openbook.openbook.basicuser.service.UserEventService;
 import com.openbook.openbook.basicuser.dto.request.EventRegistrationRequest;
+import com.openbook.openbook.global.dto.PageResponse;
 import com.openbook.openbook.global.dto.ResponseMessage;
+import com.openbook.openbook.global.dto.SliceResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,5 +38,11 @@ public class UserEventController {
     @GetMapping("/events/{eventId}/layout/status")
     public ResponseEntity<EventLayoutStatus> getEventLayoutStatus(@PathVariable Long eventId) {
         return ResponseEntity.ok(userEventService.getEventLayoutStatus(eventId));
+    }
+
+    @GetMapping("/events")
+    public ResponseEntity<SliceResponse<EventBasicData>> getEvents(@RequestParam(defaultValue = "all") String progress,
+                                                                   @PageableDefault(size = 6) Pageable pageable) {
+        return ResponseEntity.ok(SliceResponse.of(userEventService.getEventBasicData(pageable, progress)));
     }
 }
--- src/main/java/com/openbook/openbook/event/repository/EventRepository.java
+++ src/main/java/com/openbook/openbook/event/repository/EventRepository.java
@@ -2,8 +2,10 @@ package com.openbook.openbook.event.repository;
 
 import com.openbook.openbook.event.entity.Event;
 import com.openbook.openbook.event.dto.EventStatus;
+import java.time.LocalDate;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -19,5 +21,19 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     @Query("SELECT e FROM Event e WHERE e.status=:status ORDER BY e.registeredAt")
     Page<Event> findAllRequestedByStatus(Pageable pageable, EventStatus status);
+
+    // USER
+    @Query("SELECT e FROM Event e WHERE e.status = 'APPROVE'")
+    Slice<Event> findAllApproved(Pageable pageable);
+
+    @Query("SELECT e FROM Event e WHERE e.status = 'APPROVE' AND current_date BETWEEN e.openDate AND e.closeDate")
+    Slice<Event> findAllOnGoing(Pageable pageable);
+
+    @Query("SELECT e FROM Event e WHERE e.status = 'APPROVE' "
+            + "AND current_date BETWEEN e.boothRecruitmentStartDate AND e.boothRecruitmentEndDate")
+    Slice<Event> findAllRecruiting(Pageable pageable);
+
+    @Query("SELECT e FROM Event e WHERE e.status = 'APPROVE' AND e.closeDate <= current_date")
+    Slice<Event> findAllTerminated(Pageable pageable);
 
 }

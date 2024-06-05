0370049d6358da3fa6aba90b044cd28b33f613ae
gitseoyeon <tjdus553926@gmail.com>
2024-06-06 00:15:36 +0900

    Fix: 부스 상세 정보 추가 및 시간 포멧팅

--- src/main/java/com/openbook/openbook/basicuser/dto/response/BoothDetail.java
+++ src/main/java/com/openbook/openbook/basicuser/dto/response/BoothDetail.java
@@ -1,24 +1,30 @@
 package com.openbook.openbook.basicuser.dto.response;
 
 import com.openbook.openbook.booth.entity.Booth;
-import com.openbook.openbook.event.entity.Event;
+import com.openbook.openbook.event.entity.EventLayoutArea;
+import com.openbook.openbook.eventmanager.dto.BoothAreaData;
 
-import static com.openbook.openbook.global.util.Formatter.getFormattingDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static com.openbook.openbook.global.util.Formatter.getFormattingTime;
 
 public record BoothDetail(
         Long id,
         String name,
-        String openDate,
-        String closeDate,
+        String openTime,
+        String closeTime,
+        List<BoothAreaData> location,
         String description,
         String mainImageUrl
 ) {
-    public static BoothDetail of(Booth booth, Event event){
+    public static BoothDetail of(Booth booth, List<BoothAreaData> boothAreaData){
         return new BoothDetail(
                 booth.getId(),
                 booth.getName(),
-                getFormattingDate(event.getOpenDate().atStartOfDay()),
-                getFormattingDate(event.getCloseDate().atStartOfDay()),
+                getFormattingTime(booth.getOpenTime()),
+                getFormattingTime(booth.getCloseTime()),
+                boothAreaData,
                 booth.getDescription(),
                 booth.getMainImageUrl()
         );

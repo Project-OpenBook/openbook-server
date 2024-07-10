20287ec4d9d9e67ee252fd21b471634113ddeefa
siwon <muncool39@gmail.com>
2024-07-10 20:02:03 +0900

    Feat: 부스 승인 및 거절시의 알림

--- src/main/java/com/openbook/openbook/user/dto/AlarmType.java
+++ src/main/java/com/openbook/openbook/user/dto/AlarmType.java
@@ -11,6 +11,10 @@ public enum AlarmType {
     EVENT_REQUEST("행사 등록이 요청되었습니다."),
     BOOTH("부스"),
     BOOTH_REQUEST("부스 등록이 요청되었습니다.");
+    BOOTH_APPROVED("부스 등록이 승인되었습니다."),
+    BOOTH_REJECTED("부스 등록이 거절되었습니다.")
+
+    ;
 
     private final String message;
 }

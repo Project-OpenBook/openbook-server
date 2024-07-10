ba80e4d1a04b07a98f4d21b738f2d8ec2bfc08ab
siwon <muncool39@gmail.com>
2024-07-10 20:55:59 +0900

    Style: 공백 삭제 및 줄바꿈

--- src/main/java/com/openbook/openbook/user/dto/AlarmType.java
+++ src/main/java/com/openbook/openbook/user/dto/AlarmType.java
@@ -7,10 +7,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum AlarmType {
 
-    EVENT("행사"),
     EVENT_REQUEST("행사 등록이 요청되었습니다."),
-    BOOTH("부스"),
-    BOOTH_REQUEST("부스 등록이 요청되었습니다.");
+    BOOTH_REQUEST("부스 등록이 요청되었습니다."),
+
     EVENT_APPROVED("행사 등록이 승인되었습니다."),
     EVENT_REJECTED("행사 등록이 거부되었습니다."),
 

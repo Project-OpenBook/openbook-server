da2fae2cf6a34866d7d24d27349c7d5896a85f69
siwon <muncool39@gmail.com>
2024-07-16 22:59:01 +0900

    Fix: 태그 관련 엔티티 수정

--- src/main/java/com/openbook/openbook/event/entity/EventTag.java
+++ src/main/java/com/openbook/openbook/event/entity/EventTag.java
@@ -1,10 +1,13 @@
 package com.openbook.openbook.event.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -16,6 +19,15 @@ public class EventTag {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
 
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Event linkedEvent;
+
+    @Builder
+    public EventTag(String content, Event linkedEvent) {
+        this.content = content;
+        this.linkedEvent = linkedEvent;
+    }
 }

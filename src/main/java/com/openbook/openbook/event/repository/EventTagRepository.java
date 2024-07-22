b2e05c905fda148eac4c61b757878570d1c0bf76
siwon <muncool39@gmail.com>
2024-07-22 12:31:00 +0900

    Fix: 행사 응답에 태그 목록이 추가되도록 수정

--- src/main/java/com/openbook/openbook/event/repository/EventTagRepository.java
+++ src/main/java/com/openbook/openbook/event/repository/EventTagRepository.java
@@ -1,9 +1,14 @@
 package com.openbook.openbook.event.repository;
 
 import com.openbook.openbook.event.entity.EventTag;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventTagRepository extends JpaRepository<EventTag, Long> {
+    @Query("SELECT t FROM EventTag t WHERE t.linkedEvent.id=:eventId")
+    List<EventTag> findAllByLinkedEventId(Long eventId);
+
 }

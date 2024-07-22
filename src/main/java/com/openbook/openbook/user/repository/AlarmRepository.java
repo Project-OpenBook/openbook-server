326956fa6210d61fcc970033e811a5b07b87b8c6
siwon <muncool39@gmail.com>
2024-07-22 11:05:06 +0900

    Add: 삭제 쿼리 추가

--- src/main/java/com/openbook/openbook/user/repository/AlarmRepository.java
+++ src/main/java/com/openbook/openbook/user/repository/AlarmRepository.java
@@ -7,6 +7,8 @@ import java.util.Optional;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,5 +16,8 @@ public interface AlarmRepository extends JpaRepository<Alarm, Long> {
 
     Optional<Alarm> findById(Long id);
     Slice<Alarm> findAllByReceiver(Pageable pageable, User receiver);
+    @Modifying
+    @Query("DELETE FROM Alarm a WHERE a.receiver.id=:receiverId")
+    void deleteAllByReceiverId(Long receiverId);
 
 }

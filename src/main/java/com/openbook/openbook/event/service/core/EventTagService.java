8cf6ff21cde1e0f57b1307391d223d56f11d6c9c
siwon <muncool39@gmail.com>
2024-08-01 14:48:26 +0900

    Feat: 태그의 이름과 행사 상태로 태그와 연결된 행사를 찾는 기능

--- src/main/java/com/openbook/openbook/event/service/core/EventTagService.java
+++ src/main/java/com/openbook/openbook/event/service/core/EventTagService.java
@@ -3,9 +3,12 @@ package com.openbook.openbook.event.service.core;
 
 import com.openbook.openbook.event.entity.Event;
 import com.openbook.openbook.event.entity.EventTag;
+import com.openbook.openbook.event.entity.dto.EventStatus;
 import com.openbook.openbook.event.repository.EventTagRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -26,4 +29,9 @@ public class EventTagService {
     public List<EventTag> getEventTags(Long eventId) {
         return eventTagRepository.findAllByLinkedEventId(eventId);
     }
+
+    public Slice<Event> getEventsWithTagNameMatchBy(String name, EventStatus status, Pageable pageable) {
+        return eventTagRepository.findLinkedEventsByNameAndEventStatus(pageable, name, status);
+    }
+
 }

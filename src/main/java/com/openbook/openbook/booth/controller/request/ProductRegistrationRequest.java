43e96467d9297bad876d5b1f1b5b2d1b9ac273d0
siwon <muncool39@gmail.com>
2024-08-14 15:10:53 +0900

    Add: 상품 생성 요청 객체

--- src/main/java/com/openbook/openbook/booth/controller/request/ProductRegistrationRequest.java
+++ src/main/java/com/openbook/openbook/booth/controller/request/ProductRegistrationRequest.java
@@ -1,4 +1,15 @@
 package com.openbook.openbook.booth.controller.request;
 
-public record ProductRegistrationRequest() {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
+
+public record ProductRegistrationRequest(
+        @NotBlank String name,
+        String description,
+        int stock,
+        int price,
+        @Size(max = 5) List<MultipartFile> images
+) {
 }

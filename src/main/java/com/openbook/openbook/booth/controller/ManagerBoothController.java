b2b97f07c1281f199a8938423ff9a9b8c342d257
siwon <muncool39@gmail.com>
2024-08-14 15:15:11 +0900

    Feat: 부스 상품 추가 API

--- src/main/java/com/openbook/openbook/booth/controller/ManagerBoothController.java
+++ src/main/java/com/openbook/openbook/booth/controller/ManagerBoothController.java
@@ -1,9 +1,11 @@
 package com.openbook.openbook.booth.controller;
 
+import com.openbook.openbook.booth.controller.request.ProductRegistrationRequest;
 import com.openbook.openbook.booth.controller.response.BoothManageData;
 import com.openbook.openbook.booth.service.ManagerBoothService;
 import com.openbook.openbook.global.dto.ResponseMessage;
 import com.openbook.openbook.global.dto.SliceResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -33,4 +36,14 @@ public class ManagerBoothController {
         managerBoothService.deleteBooth(Long.valueOf(authentication.getName()), boothId);
         return ResponseEntity.ok(new ResponseMessage("부스를 삭제했습니다."));
     }
+
+    @PostMapping("/booths/{booth_id}/products")
+    public ResponseEntity<ResponseMessage> addProduct(Authentication authentication,
+                                                      @PathVariable Long booth_id,
+                                                      @Valid ProductRegistrationRequest request){
+        managerBoothService.addBoothProduct(Long.valueOf(authentication.getName()), booth_id, request);
+        return ResponseEntity.ok(new ResponseMessage("상품 추가에 성공했습니다."));
+    }
+
+
 }

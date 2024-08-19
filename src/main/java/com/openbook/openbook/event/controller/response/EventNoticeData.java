cda25f522086030025d99e71de1baf7c8d6e20b9
siwon <muncool39@gmail.com>
2024-08-20 00:05:40 +0900

    Fix: 공지 응답 객체 필드 추가

--- src/main/java/com/openbook/openbook/event/controller/response/EventNoticeData.java
+++ src/main/java/com/openbook/openbook/event/controller/response/EventNoticeData.java
@@ -5,19 +5,25 @@ import com.openbook.openbook.event.entity.dto.EventNoticeType;
 import java.time.LocalDateTime;
 
 public record EventNoticeData(
+        long id,
         String title,
         String content,
         String imageUrl,
         EventNoticeType type,
-        LocalDateTime registeredAt
+        LocalDateTime registeredAt,
+        long eventId,
+        String eventName
 ) {
     public static EventNoticeData of(EventNotice notice) {
         return new EventNoticeData(
+                notice.getId(),
                 notice.getTitle(),
                 notice.getContent(),
                 notice.getImageUrl(),
                 EventNoticeType.valueOf(notice.getType()),
-                notice.getRegisteredAt()
+                notice.getRegisteredAt(),
+                notice.getLinkedEvent().getId(),
+                notice.getLinkedEvent().getName()
         );
     }
 }

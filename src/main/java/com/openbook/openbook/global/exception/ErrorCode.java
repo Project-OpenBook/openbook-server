42341d406e783e6706816aa525fc56db91235ae6
siwon <muncool39@gmail.com>
2024-08-20 00:05:18 +0900

    Add: 행사 공지가 없는 경우의 에러 코드 추가

--- src/main/java/com/openbook/openbook/global/exception/ErrorCode.java
+++ src/main/java/com/openbook/openbook/global/exception/ErrorCode.java
@@ -35,6 +35,7 @@ public enum ErrorCode {
     // NOT FOUND
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저 정보를 찾을 수 없습니다."),
     EVENT_NOT_FOUND(HttpStatus.NOT_FOUND, "행사 정보를 찾을 수 없습니다."),
+    EVENT_NOTICE_NOT_FOUND(HttpStatus.NOT_FOUND, "행사 공지 정보를 찾을 수 없습니다."),
     BOOTH_NOT_FOUND(HttpStatus.NOT_FOUND, "부스 정보를 찾을 수 없습니다."),
     AREA_NOT_FOUND(HttpStatus.NOT_FOUND, "구역 정보를 찾을 수 없습니다."),
     ALARM_NOT_FOUND(HttpStatus.NOT_FOUND, "알림 정보를 찾을 수 없습니다."),

73ef5fb0362aba189864322cc0db35d1453a720c
siwon <muncool39@gmail.com>
2024-08-22 00:14:31 +0900

    Add: 부스 상품 dto 에 상품 이미지, 카테고리 id 필드 추가

--- src/main/java/com/openbook/openbook/booth/dto/BoothProductDto.java
+++ src/main/java/com/openbook/openbook/booth/dto/BoothProductDto.java
@@ -1,12 +1,15 @@
 package com.openbook.openbook.booth.dto;
 
-import com.openbook.openbook.booth.entity.Booth;
+import com.openbook.openbook.booth.entity.BoothProductCategory;
+import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
 
 public record BoothProductDto(
         String name,
         String description,
         int stock,
         int price,
-        Booth linkedBooth
+        List<MultipartFile> images,
+        BoothProductCategory linkedCategory
 ) {
 }

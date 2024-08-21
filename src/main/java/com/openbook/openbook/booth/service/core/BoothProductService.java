34de2c49165ad03900b115de3e4a12f870020728
siwon <muncool39@gmail.com>
2024-08-22 00:03:05 +0900

    Add: 상품 카테고리 생성 메서드

--- src/main/java/com/openbook/openbook/booth/service/core/BoothProductService.java
+++ src/main/java/com/openbook/openbook/booth/service/core/BoothProductService.java
@@ -3,7 +3,9 @@ package com.openbook.openbook.booth.service.core;
 
 import com.openbook.openbook.booth.dto.BoothProductDto;
 import com.openbook.openbook.booth.entity.BoothProduct;
+import com.openbook.openbook.booth.entity.BoothProductCategory;
 import com.openbook.openbook.booth.entity.BoothProductImage;
+import com.openbook.openbook.booth.repository.BoothProductCategoryRepository;
 import com.openbook.openbook.booth.repository.BoothProductImageRepository;
 import com.openbook.openbook.booth.repository.BoothProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,8 +18,17 @@ public class BoothProductService {
     private final BoothProductRepository boothProductRepository;
     private final BoothProductImageRepository boothProductImageRepository;
 
-    public BoothProduct createBoothProduct(final BoothProductDto boothProduct) {
-        return boothProductRepository.save(
+    public void createProductCategory(String categoryName, Booth linkedBooth) {
+        categoryRepository.save(
+                BoothProductCategory.builder()
+                        .name(categoryName)
+                        .linkedBooth(linkedBooth)
+                        .build()
+        );
+    }
+
+    public void createBoothProduct(final BoothProductDto boothProduct) {
+        BoothProduct product = boothProductRepository.save(
                 BoothProduct.builder()
                         .name(boothProduct.name())
                         .description(boothProduct.description())

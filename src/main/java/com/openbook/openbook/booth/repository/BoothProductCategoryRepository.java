da5ae64985e7f2fdc5505606c55f94dfc6b16e57
siwon <muncool39@gmail.com>
2024-08-23 22:22:48 +0900

    Add: 연결된 부스 id로 카테고리 리스트 찾는 쿼리

--- src/main/java/com/openbook/openbook/booth/repository/BoothProductCategoryRepository.java
+++ src/main/java/com/openbook/openbook/booth/repository/BoothProductCategoryRepository.java
@@ -2,9 +2,12 @@ package com.openbook.openbook.booth.repository;
 
 
 import com.openbook.openbook.booth.entity.BoothProductCategory;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoothProductCategoryRepository extends JpaRepository<BoothProductCategory, Long> {
+
+    List<BoothProductCategory> findAllByLinkedBoothId(Long linkedBoothId);
 }

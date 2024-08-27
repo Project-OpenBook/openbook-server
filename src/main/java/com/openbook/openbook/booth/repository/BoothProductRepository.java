0b9f1c84fdca6c02f907296542d6aeb725168e27
siwon <muncool39@gmail.com>
2024-08-28 01:49:01 +0900

    Add: 연결된 카테고리 id로 상품 목록을 찾는 쿼리

--- src/main/java/com/openbook/openbook/booth/repository/BoothProductRepository.java
+++ src/main/java/com/openbook/openbook/booth/repository/BoothProductRepository.java
@@ -1,7 +1,12 @@
 package com.openbook.openbook.booth.repository;
 
 import com.openbook.openbook.booth.entity.BoothProduct;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoothProductRepository extends JpaRepository<BoothProduct, Long> {
+
+    Slice<BoothProduct> findAllByLinkedCategoryId(Long linkedCategoryId, Pageable pageable);
+
 }

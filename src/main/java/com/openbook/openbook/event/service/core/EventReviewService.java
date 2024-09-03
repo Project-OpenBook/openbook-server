ad34215fe82c3183bb2ca1534e192cf83d7bb0b2
siwon <muncool39@gmail.com>
2024-09-03 23:03:10 +0900

    Add: 행사 리뷰 id로 행사 리뷰를 찾는 메서드

--- src/main/java/com/openbook/openbook/event/service/core/EventReviewService.java
+++ src/main/java/com/openbook/openbook/event/service/core/EventReviewService.java
@@ -6,6 +6,8 @@ import com.openbook.openbook.event.entity.EventReview;
 import com.openbook.openbook.event.entity.EventReviewImage;
 import com.openbook.openbook.event.repository.EventReviewImageRepository;
 import com.openbook.openbook.event.repository.EventReviewRepository;
+import com.openbook.openbook.global.exception.ErrorCode;
+import com.openbook.openbook.global.exception.OpenBookException;
 import com.openbook.openbook.global.util.S3Service;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +23,12 @@ public class EventReviewService {
     private final EventReviewRepository eventReviewRepository;
     private final EventReviewImageRepository eventReviewImageRepository;
     private final S3Service s3Service;
+
+    public EventReview getEventReviewOrException(long eventReviewId) {
+        return eventReviewRepository.findById(eventReviewId).orElseThrow(()->
+                new OpenBookException(ErrorCode.REVIEW_NOT_FOUND)
+        );
+    }
 
     public EventReview createEventReview(EventReviewDto dto){
         return eventReviewRepository.save(

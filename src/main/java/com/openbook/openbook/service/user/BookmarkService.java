2d73df038a24f09220223e67f9c429b8ceab810b
siwon <muncool39@gmail.com>
2024-09-16 02:30:11 +0900

    Add: 생성하려는 북마크가 이미 존재하는 경우를 검사하는 코드

--- src/main/java/com/openbook/openbook/service/user/BookmarkService.java
+++ src/main/java/com/openbook/openbook/service/user/BookmarkService.java
@@ -5,7 +5,10 @@ import com.openbook.openbook.api.user.request.BookmarkRequest;
 import com.openbook.openbook.domain.user.Bookmark;
 import com.openbook.openbook.domain.user.User;
 import com.openbook.openbook.domain.user.dto.BookmarkType;
+import com.openbook.openbook.exception.ErrorCode;
+import com.openbook.openbook.exception.OpenBookException;
 import com.openbook.openbook.repository.user.BookmarkRepository;
+import java.awt.print.Book;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,14 +18,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class BookmarkService {
 
     private final UserService userService;
+
     private final BookmarkRepository bookmarkRepository;
 
     @Transactional
     public void createBookmark(long userId, BookmarkRequest request) {
         User user = userService.getUserOrException(userId);
+        BookmarkType type = BookmarkType.fromString(request.type());
+        if(bookmarkRepository.existsByUserIdAndResourceIdAndBookmarkType(userId, request.resourceId(), type)) {
+            throw new OpenBookException(ErrorCode.ALREADY_BOOKMARK);
+        }
         bookmarkRepository.save( Bookmark.builder()
                 .user(user)
-                .bookmarkType(BookmarkType.fromString(request.type()))
+                .bookmarkType(type)
                 .resourceId(request.resourceId())
                 .alarmSet(request.alarmSet() == null || request.alarmSet())
                 .build()

1bdc0a192c42f2cd5f87c4fccd675db0e7eaaea9
siwon <muncool39@gmail.com>
2024-09-16 01:23:53 +0900

    Add: 북마크 생성하는 서비스 메서드

--- src/main/java/com/openbook/openbook/service/user/BookmarkService.java
+++ src/main/java/com/openbook/openbook/service/user/BookmarkService.java
@@ -1,10 +1,31 @@
 package com.openbook.openbook.service.user;
 
 
+import com.openbook.openbook.api.user.request.BookmarkRequest;
+import com.openbook.openbook.domain.user.Bookmark;
+import com.openbook.openbook.domain.user.User;
+import com.openbook.openbook.repository.user.BookmarkRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class BookmarkService {
+
+    private final UserService userService;
+    private final BookmarkRepository bookmarkRepository;
+
+    @Transactional
+    public void createBookmark(long userId, BookmarkRequest request) {
+        User user = userService.getUserOrException(userId);
+        bookmarkRepository.save( Bookmark.builder()
+                .user(user)
+                .bookmarkType(request.type())
+                .resourceId(request.resourceId())
+                .alarmSet(request.alarmSet() == null || request.alarmSet())
+                .build()
+        );
+    }
+
 }

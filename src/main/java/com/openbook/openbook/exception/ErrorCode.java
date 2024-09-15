a049405ca2acdf32cca1b0db358b76746e527ae5
siwon <muncool39@gmail.com>
2024-09-16 02:28:31 +0900

    Add: 이미 북마크 한 내용인 경우의 에러 코드 추가

--- src/main/java/com/openbook/openbook/exception/ErrorCode.java
+++ src/main/java/com/openbook/openbook/exception/ErrorCode.java
@@ -35,6 +35,7 @@ public enum ErrorCode {
     ALREADY_RESERVED_DATE(HttpStatus.CONFLICT, "이미 존재하는 예약 날짜 입니다."),
     DUPLICATE_RESERVED_TIME(HttpStatus.CONFLICT, "중복 되는 시간 데이터가 있습니다."),
     ALREADY_RESERVED_SERVICE(HttpStatus.CONFLICT, "이미 예약된 시간 입니다."),
+    ALREADY_BOOKMARK(HttpStatus.CONFLICT, "이미 북마크 목록에 존재합니다."),
 
     EXCEED_MAXIMUM_CATEGORY(HttpStatus.CONFLICT, "생성할 수 있는 최대 카테고리 수를 초과했습니다."),
     ALREADY_EXIST_CATEGORY(HttpStatus.CONFLICT, "이미 존재하는 카테고리 입니다."),

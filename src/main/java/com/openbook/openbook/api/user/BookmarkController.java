2c6c4a9b248b5f372ea131e5d9dd53df382a05f9
siwon <muncool39@gmail.com>
2024-09-16 01:28:25 +0900

    Add: 북마크하는 rest controller 메서드

--- src/main/java/com/openbook/openbook/api/user/BookmarkController.java
+++ src/main/java/com/openbook/openbook/api/user/BookmarkController.java
@@ -1,11 +1,28 @@
 package com.openbook.openbook.api.user;
 
 
+import com.openbook.openbook.api.ResponseMessage;
+import com.openbook.openbook.api.user.request.BookmarkRequest;
+import com.openbook.openbook.service.user.BookmarkService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class BookmarkController {
+
+    private final BookmarkService bookmarkService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/bookmark")
+    public ResponseMessage bookmark(Authentication authentication, @Valid BookmarkRequest request) {
+        bookmarkService.createBookmark(Long.parseLong(authentication.getName()), request);
+        return new ResponseMessage("북마크에 성공했습니다.");
+    }
 
 }

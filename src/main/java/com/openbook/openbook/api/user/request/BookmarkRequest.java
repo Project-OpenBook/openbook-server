dedcc030e113e183e8b8f91f9c2b1188403605f7
siwon <muncool39@gmail.com>
2024-09-16 02:10:56 +0900

    Fix: 요청 객체의 북마크 타입 자료형 변경

--- src/main/java/com/openbook/openbook/api/user/request/BookmarkRequest.java
+++ src/main/java/com/openbook/openbook/api/user/request/BookmarkRequest.java
@@ -1,10 +1,9 @@
 package com.openbook.openbook.api.user.request;
 
-import com.openbook.openbook.domain.user.dto.BookmarkType;
 import jakarta.validation.constraints.NotNull;
 
 public record BookmarkRequest(
-        @NotNull BookmarkType type,
+        @NotNull String type,
         @NotNull long resourceId,
         Boolean alarmSet
 ) {

33c691dd0f82894cb1f13195e578b5dcc4d6b0ee
siwon <muncool39@gmail.com>
2024-09-17 22:57:21 +0900

    Add: 사용자 id와 북마크 타입으로 북마크 목록을 가져오는 쿼리 메서드

--- src/main/java/com/openbook/openbook/repository/user/BookmarkRepository.java
+++ src/main/java/com/openbook/openbook/repository/user/BookmarkRepository.java
@@ -2,6 +2,8 @@ package com.openbook.openbook.repository.user;
 
 import com.openbook.openbook.domain.user.Bookmark;
 import com.openbook.openbook.domain.user.dto.BookmarkType;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,7 @@ import org.springframework.stereotype.Repository;
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
     boolean existsByUserIdAndResourceIdAndBookmarkType(long userId, long resourceId, BookmarkType type);
+
+    Slice<Bookmark> findAllByUserIdAndBookmarkType(long userId, BookmarkType type, Pageable pageable);
 
 }

03e6fa1d67da3b6af481be58d70fcc5d8aa5c268
siwon <muncool39@gmail.com>
2024-09-17 22:53:39 +0900

    Add: 북마크 목록 조회 컨트롤러 메서드 틀

--- src/main/java/com/openbook/openbook/api/user/BookmarkController.java
+++ src/main/java/com/openbook/openbook/api/user/BookmarkController.java
@@ -8,8 +8,10 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +27,12 @@ public class BookmarkController {
                                     @Valid @RequestBody BookmarkRequest request) {
         bookmarkService.createBookmark(Long.parseLong(authentication.getName()), request);
         return new ResponseMessage("북마크에 성공했습니다.");
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/bookmark")
+    public void findBookmark(Authentication authentication, @RequestParam(value = "type") String request) {
+
     }
 
 }

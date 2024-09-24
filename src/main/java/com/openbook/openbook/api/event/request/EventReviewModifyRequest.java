be753149d8eab462b85c5c5a042716f912db5b1b
siwon <muncool39@gmail.com>
2024-09-25 01:12:52 +0900

    Add: 행사 리뷰 수정 요청 객체 추가

---
+++ src/main/java/com/openbook/openbook/api/event/request/EventReviewModifyRequest.java
@@ -0,0 +1,14 @@
+package com.openbook.openbook.api.event.request;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import java.util.List;
+import org.springframework.web.multipart.MultipartFile;
+
+@NotNull
+public record EventReviewModifyRequest(
+        @Max(value = 5) float star,
+        String content,
+        @Max(value = 5) List<MultipartFile> images
+) {
+}

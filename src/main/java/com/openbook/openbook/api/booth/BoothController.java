6ffcd4e63582083d0d52cd906ba9ef5c23e14c5e
gitseoyeon <tjdus553926@gmail.com>
2024-10-02 09:40:58 +0900

    Add: 부스 수정 컨트롤러 추가

--- src/main/java/com/openbook/openbook/api/booth/BoothController.java
+++ src/main/java/com/openbook/openbook/api/booth/BoothController.java
@@ -1,6 +1,7 @@
 package com.openbook.openbook.api.booth;
 
 
+import com.openbook.openbook.api.booth.request.BoothModifyRequest;
 import com.openbook.openbook.api.booth.request.BoothRegistrationRequest;
 import com.openbook.openbook.api.booth.request.BoothStatusUpdateRequest;
 import com.openbook.openbook.api.booth.response.BoothBasicData;
@@ -24,7 +25,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -87,6 +89,15 @@ public class BoothController {
         return SliceResponse.of(
                 boothService.getBoothsByManager(Long.valueOf(authentication.getName()), pageable, status)
                         .map(BoothManageData::of));
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PatchMapping("/booths/{booth_id}")
+    public ResponseMessage modifyReview(Authentication authentication,
+                                        @PathVariable Long booth_id,
+                                        @Valid BoothModifyRequest request){
+        boothService.modifyBooth(Long.parseLong(authentication.getName()), booth_id, request);
+        return new ResponseMessage("부스 수정에 성공했습니다.");
     }
 
     @DeleteMapping("/booths/{boothId}")

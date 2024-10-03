47009177ef60ab56b3e48b0c8282946dca74aeb2
gitseoyeon <tjdus553926@gmail.com>
2024-10-03 20:42:38 +0900

    Refactor: dto 에 담긴 데이터 update 메소드에 적용

--- src/main/java/com/openbook/openbook/domain/booth/Booth.java
+++ src/main/java/com/openbook/openbook/domain/booth/Booth.java
@@ -4,6 +4,7 @@ import com.openbook.openbook.domain.booth.dto.BoothStatus;
 import com.openbook.openbook.domain.event.Event;
 import com.openbook.openbook.domain.EntityBasicTime;
 import com.openbook.openbook.domain.user.User;
+import com.openbook.openbook.service.booth.dto.BoothUpdateData;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -77,15 +78,29 @@ public class Booth extends EntityBasicTime {
 
     public void updateStatus(BoothStatus status){ this.status = status; }
 
-    public void updateBooth(String name, String description, String mainImageUrl,
-                            String accountNumber, String accountBankName, LocalDateTime openTime, LocalDateTime closeTime){
-        this.name = name;
-        this.description = description;
-        this.mainImageUrl = mainImageUrl;
-        this.accountNumber = accountNumber;
-        this.accountBankName = accountBankName;
-        this.openTime = openTime;
-        this.closeTime = closeTime;
+    public void updateBooth(BoothUpdateData updateData){
+        if(updateData.name() != null){
+            this.name = updateData.name();
+        }
+        if(updateData.description() != null){
+            this.description = updateData.description();
+        }
+        if(updateData.openTime() != null){
+            this.openTime = updateData.openTime();
+        }
+        if(updateData.closeTime() != null){
+            this.closeTime = updateData.closeTime();
+        }
+        if(updateData.mainImage() != null){
+            this.mainImageUrl = updateData.mainImage();
+        }
+        if(updateData.accountBankName() != null){
+            this.accountBankName = updateData.accountBankName();
+        }
+        if(updateData.accountNumber() != null){
+            this.accountNumber = updateData.accountNumber();
+        }
+
     }
 
     @Builder

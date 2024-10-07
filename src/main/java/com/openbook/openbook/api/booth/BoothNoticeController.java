83df134c8411f70d5a054f23472bc31f244d4a75
gitseoyeon <tjdus553926@gmail.com>
2024-10-08 01:19:07 +0900

    Feat: 부스 수정 컨트롤러 추가

--- src/main/java/com/openbook/openbook/api/booth/BoothNoticeController.java
+++ src/main/java/com/openbook/openbook/api/booth/BoothNoticeController.java
@@ -1,12 +1,14 @@
 package com.openbook.openbook.api.booth;
 
 
+import com.openbook.openbook.api.booth.request.BoothNoticeModifyRequest;
 import com.openbook.openbook.api.booth.request.BoothNoticeRegisterRequest;
 import com.openbook.openbook.api.booth.response.BoothNoticeResponse;
 import com.openbook.openbook.service.booth.BoothNoticeService;
 import com.openbook.openbook.api.ResponseMessage;
 import com.openbook.openbook.api.SliceResponse;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -14,8 +16,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -40,6 +44,15 @@ public class BoothNoticeController {
     @GetMapping("/booths/notices/{noticeId}")
     public ResponseEntity<BoothNoticeResponse> getBoothNotice(@PathVariable Long noticeId){
         return ResponseEntity.ok(BoothNoticeResponse.of(boothNoticeService.getBoothNotice(noticeId)));
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PatchMapping("/booths/notices/{notice_id}")
+    public ResponseMessage modifyNotice(Authentication authentication,
+                                        @PathVariable Long notice_id,
+                                        @NotNull BoothNoticeModifyRequest request){
+        boothNoticeService.modifyNotice(Long.parseLong(authentication.getName()), notice_id, request);
+        return new ResponseMessage("공지 수정에 성공했습니다.");
     }
 
 }

46abb5cecffd64cd01310f860a377677b4ee481a
siwon <muncool39@gmail.com>
2024-10-09 02:04:30 +0900

    Refactor: BoothProductController

--- src/main/java/com/openbook/openbook/api/booth/BoothProductController.java
+++ src/main/java/com/openbook/openbook/api/booth/BoothProductController.java
@@ -12,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,25 +27,48 @@ public class BoothProductController {
 
     private final BoothProductService boothProductService;
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/booths/{booth_id}/product-category")
-    public ResponseEntity<ResponseMessage> addProductCategory(Authentication authentication,
-                                                              @PathVariable Long booth_id,
-                                                              @Valid ProductCategoryRegister request) {
+    public ResponseMessage addProductCategory(Authentication authentication,
+                                              @PathVariable Long booth_id,
+                                              @Valid ProductCategoryRegister request) {
         boothProductService.addProductCategory(Long.valueOf(authentication.getName()), booth_id, request);
-        return ResponseEntity.ok(new ResponseMessage("상품 카테고리 생성에 성공했습니다."));
+        return new ResponseMessage("상품 카테고리 생성에 성공했습니다.");
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/booths/{booth_id}/products")
-    public ResponseEntity<ResponseMessage> addProduct(Authentication authentication,
-                                                      @PathVariable Long booth_id,
-                                                      @Valid ProductRegistrationRequest request){
+    public ResponseMessage addProduct(Authentication authentication,
+                                      @PathVariable Long booth_id,
+                                      @Valid ProductRegistrationRequest request){
         boothProductService.addBoothProduct(Long.valueOf(authentication.getName()), booth_id, request);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseMessage("상품 추가에 성공했습니다."));
+        return new ResponseMessage("상품 추가에 성공했습니다.");
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/booths/{booth_id}/products")
+    public List<CategoryProductsResponse> getAllBoothProducts(@PathVariable Long booth_id,
+                                                              @PageableDefault(size = 5) Pageable pageable) {
+        return boothProductService.findAllBoothProducts(booth_id, pageable);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/booths/products/category")
+    public CategoryProductsResponse getProductsByCategory(@RequestParam Long category_id,
+                                                          @PageableDefault(size = 5) Pageable pageable) {
+        return boothProductService.findCategoryProducts(category_id, pageable);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping("/booths/{booth_id}/product-category")
+    public List<ProductCategoryResponse> getProductCategory(@PathVariable Long booth_id) {
+        return boothProductService.getProductCategoryResponseList(booth_id);
     }
 
     @ResponseStatus(HttpStatus.OK)
     @DeleteMapping("/booths/products/{product_id}")
-    public ResponseMessage deleteProduct(Authentication authentication, @PathVariable Long product_id) {
+    public ResponseMessage deleteProduct(Authentication authentication,
+                                         @PathVariable Long product_id) {
         boothProductService.deleteProduct(Long.parseLong(authentication.getName()), product_id);
         return new ResponseMessage("상품 삭제에 성공했습니다.");
     }
@@ -58,23 +80,6 @@ public class BoothProductController {
                                                  @RequestParam(defaultValue = "false") String deleteProducts) {
         boothProductService.deleteCategory(Long.parseLong(authentication.getName()), category_id, deleteProducts);
         return new ResponseMessage("상품 카테고리 삭제에 성공했습니다.");
-    }
-
-    @GetMapping("/booths/{booth_id}/product-category")
-    public ResponseEntity<List<ProductCategoryResponse>> getProductCategory(@PathVariable Long booth_id) {
-        return ResponseEntity.ok(boothProductService.getProductCategoryResponseList(booth_id));
-    }
-
-    @GetMapping("/booths/{booth_id}/products")
-    public ResponseEntity<List<CategoryProductsResponse>> getAllBoothProducts(@PathVariable Long booth_id,
-                                                                              @PageableDefault(size = 5) Pageable pageable) {
-        return ResponseEntity.ok(boothProductService.findAllBoothProducts(booth_id, pageable));
-    }
-
-    @GetMapping("/booths/products/category")
-    public ResponseEntity<CategoryProductsResponse> getProductsByCategory(@RequestParam Long category_id,
-                                                                          @PageableDefault(size = 5) Pageable pageable) {
-        return ResponseEntity.ok(boothProductService.findCategoryProducts(category_id, pageable));
     }
 
 }

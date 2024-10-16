560267ca1c7f4c56eddf6f1ed45a3f295e94230d
siwon <muncool39@gmail.com>
2024-10-16 10:44:02 +0900

    Add: 이벤트 정보를 받는 파라미터 추가

--- src/main/java/com/openbook/openbook/api/booth/BoothController.java
+++ src/main/java/com/openbook/openbook/api/booth/BoothController.java
@@ -45,11 +45,8 @@ public class BoothController {
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/booths")
-    public SliceResponse<BoothBasicData> getBooths(@PageableDefault(size = 6) Pageable pageable){
-        return SliceResponse.of(
-                boothService.getBooths(pageable)
-                        .map(BoothBasicData::of)
-        );
+    public SliceResponse<BoothBasicData> getBooths(@PageableDefault(size = 6) Pageable pageable,
+                                                   @RequestParam(defaultValue = "ALL") String event){
     }
 
     @ResponseStatus(HttpStatus.OK)

60108ca41eb21f3ada48bc41f5de58a9981ecb9e
gitseoyeon <tjdus553926@gmail.com>
2024-10-22 16:48:27 +0900

    Feat: reservationDetail 존재 여부 판별 메소드 추가

--- src/main/java/com/openbook/openbook/repository/booth/BoothReservationDetailRepository.java
+++ src/main/java/com/openbook/openbook/repository/booth/BoothReservationDetailRepository.java
@@ -1,5 +1,6 @@
 package com.openbook.openbook.repository.booth;
 
+import com.openbook.openbook.domain.booth.BoothReservation;
 import com.openbook.openbook.domain.booth.BoothReservationDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,4 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BoothReservationDetailRepository extends JpaRepository<BoothReservationDetail, Long> {
+    boolean existsByLinkedReservationAndTime(BoothReservation boothReservation, String time);
 }

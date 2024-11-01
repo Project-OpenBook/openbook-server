40a2aef2ffc0fc78d53a1648ebfeb2d045879a33
gitseoyeon <tjdus553926@gmail.com>
2024-11-02 02:12:49 +0900

    Refactor: 날짜와 시간을 하나의 그룹으로 묶어서 응답

--- src/main/java/com/openbook/openbook/api/booth/response/BoothReserveManageResponse.java
+++ src/main/java/com/openbook/openbook/api/booth/response/BoothReserveManageResponse.java
@@ -1,28 +1,28 @@
 package com.openbook.openbook.api.booth.response;
 
-import static com.openbook.openbook.util.Formatter.getFormattingDate;
-
+import com.openbook.openbook.service.booth.dto.BoothReservationDateDto;
 import com.openbook.openbook.service.booth.dto.BoothReservationDto;
+import com.openbook.openbook.util.Formatter;
 import java.util.List;
 
 public record BoothReserveManageResponse(
         long id,
         String name,
         String description,
-        String date,
         int price,
         String imageUrl,
-        List<BoothReserveDetailManageResponse> detailManageResponses
+        List<BoothReservationDateDto> dates
 ) {
     public static BoothReserveManageResponse of(BoothReservationDto reservation){
         return new BoothReserveManageResponse(
                 reservation.id(),
                 reservation.name(),
                 reservation.description(),
-                getFormattingDate(reservation.date().atStartOfDay()),
                 reservation.price(),
                 reservation.imageUrl(),
-                reservation.details().stream().map(BoothReserveDetailManageResponse::of).toList()
+                reservation.groupedDetails().entrySet().stream()
+                        .map(entry -> new BoothReservationDateDto(Formatter.getFormattingDate(entry.getKey().atStartOfDay()), entry.getValue()))
+                        .toList()
         );
     }
 }

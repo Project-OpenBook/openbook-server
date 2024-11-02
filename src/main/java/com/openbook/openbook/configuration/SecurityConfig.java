8033abd90e1bd25fe3cb64b6f8bffc171955c0ae
gitseoyeon <tjdus553926@gmail.com>
2024-11-02 20:50:56 +0900

    Add: permitPaths 추가

--- src/main/java/com/openbook/openbook/configuration/SecurityConfig.java
+++ src/main/java/com/openbook/openbook/configuration/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
     private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
 
     private final String[] permitPaths = {
+            "/actuator/health",
             "/login",
             "/signup",
             "/booths",
